b74b8def3bb25fc76f7487cd0ef89edb4449d406
Brawdunoir <yann.lacroix.dev@gmail.com>
2024-02-24 00:27:03 +0100

    ✨ add helpers functions
    Signed-off-by: Brawdunoir <yann.lacroix.dev@gmail.com>

--- player.go
+++ player.go
@@ -8,6 +8,41 @@ type Player struct {
 	IsReady bool
 }
 
+// toggleReady toggles the player's ready status.
 func (p *Player) toggleReady() {
 	p.IsReady = !p.IsReady
+}
+
+// hasPriorityCard checks if the player has a priority card on one of his species.
+func (p *Player) hasPriorityCard() bool {
+	for _, species := range p.Species {
+		if species.hasPriorityCard() {
+			return true
+		}
+	}
+	return false
+}
+
+// areSpeciesFed checks if all species are fed.
+func (p *Player) areSpeciesFed() bool {
+	for _, species := range p.Species {
+		if !species.isFed() {
+			return false
+		}
+	}
+	return true
+}
+
+// arePlayersReady checks if players are ready.
+// If priority is true, it will only check status of players with priority cards equipped.
+func arePlayersReady(players []Player, priority bool) bool {
+	for _, player := range players {
+		if priority && !player.hasPriorityCard() {
+			continue
+		}
+		if !player.IsReady {
+			return false
+		}
+	}
+	return true
 }
--- player_test.go
+++ player_test.go
@@ -21,3 +21,67 @@ func TestToggleReady(t *testing.T) {
 		t.Errorf("toggleReady(). Got %v, want %v", got, want)
 	}
 }
+
+func TestPlayerHasPriorityCard(t *testing.T) {
+	player := Player{Species: []Species{{Traits: []Card{{IsPrior: false}, {IsPrior: true}, {IsPrior: false}}}}}
+	want := true
+	got := player.hasPriorityCard()
+	if got != want {
+		t.Errorf("hasPriorityCard(). Got %v, want %v", got, want)
+	}
+
+	player = Player{Species: []Species{{Traits: []Card{{IsPrior: false}, {IsPrior: false}, {IsPrior: false}}}}}
+	want = false
+	got = player.hasPriorityCard()
+	if got != want {
+		t.Errorf("hasPriorityCard(). Got %v, want %v", got, want)
+	}
+}
+
+func TestAreSpeciesFed(t *testing.T) {
+	player := Player{Species: []Species{{Food: 3, Population: 3}, {Food: 3, Population: 3}}}
+	want := true
+	got := player.areSpeciesFed()
+	if got != want {
+		t.Errorf("areSpeciesFed(). Got %v, want %v", got, want)
+	}
+
+	player = Player{Species: []Species{{Food: 3, Population: 3}, {Food: 3, Population: 4}}}
+	want = false
+	got = player.areSpeciesFed()
+	if got != want {
+		t.Errorf("areSpeciesFed(). Got %v, want %v", got, want)
+	}
+}
+
+func TestArePlayersReady(t *testing.T) {
+	players := []Player{{IsReady: true}, {IsReady: true}, {IsReady: true}}
+	want := true
+	got := arePlayersReady(players, false)
+	if got != want {
+		t.Errorf("arePlayersReady(). Got %v, want %v", got, want)
+	}
+
+	players = []Player{{IsReady: true}, {IsReady: false}, {IsReady: true}}
+	want = false
+	got = arePlayersReady(players, false)
+	if got != want {
+		t.Errorf("arePlayersReady(). Got %v, want %v", got, want)
+	}
+}
+
+func TestArePlayersReadyPriority(t *testing.T) {
+	players := []Player{{IsReady: false, Species: []Species{{Traits: []Card{{IsPrior: true}}}}}, {IsReady: true, Species: []Species{{Traits: []Card{{IsPrior: false}}}}}}
+	want := false
+	got := arePlayersReady(players, true)
+	if got != want {
+		t.Errorf("arePlayersReady(). Got %v, want %v", got, want)
+	}
+
+	players = []Player{{IsReady: true, Species: []Species{{Traits: []Card{{IsPrior: false}}}}}, {IsReady: true, Species: []Species{{Traits: []Card{{IsPrior: false}}}}}}
+	want = true
+	got = arePlayersReady(players, true)
+	if got != want {
+		t.Errorf("arePlayersReady(). Got %v, want %v", got, want)
+	}
+}
